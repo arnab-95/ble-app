df6e126fa0c1c8bc8577aa64c30f34ffdeaa1331
arnab <arnab.prophet@gmail.com>
2023-12-13 13:18:34 -0500

    BLE Module updated

--- windows/BLEModule.cs
+++ windows/BLEModule.cs
@@ -1,8 +1,50 @@
-﻿using System;
+﻿using Microsoft.ReactNative.Managed;
+using System;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
 
-public class Class1
+namespace MyBLEApp.Modules
 {
-	public Class1()
-	{
-	}
+    [ReactModule("BLEModule")]
+    class BLEModule
+    {
+        private BluetoothLEAdvertisementWatcher _watcher;
+
+        public BLEModule()
+        {
+            _watcher = new BluetoothLEAdvertisementWatcher
+            {
+                ScanningMode = BluetoothLEScanningMode.Active
+            };
+
+            _watcher.Received += OnAdvertisementReceived;
+            _watcher.Stopped += OnAdvertisementWatcherStopped;
+        }
+
+        private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            // TODO: Handle received advertisement
+            // You can access advertisement data from args.Advertisement
+        }
+
+        private void OnAdvertisementWatcherStopped(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementWatcherStoppedEventArgs args)
+        {
+            // TODO: Handle watcher stopped
+            // You can check the status from args.Error
+        }
+
+        [ReactMethod("startScanning")]
+        public void StartScanning()
+        {
+            _watcher.Start();
+        }
+
+        [ReactMethod("stopScanning")]
+        public void StopScanning()
+        {
+            _watcher.Stop();
+        }
+
+        // Additional methods for connecting to devices, reading/writing characteristics, etc.
+    }
 }
